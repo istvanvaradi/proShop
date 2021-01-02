6c7dce5bdbc6641fb2742fb849280ccfa6c08860
Istvan Varadi <Istvan@Amsos-MacBook-Pro.local>
2021-01-02 01:29:24 +0100

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-import react from "react";
+import React from "react";
+import Header from "./Components/Header"
+import Footer from "./Components/Footer" 
+import {Container} from "react-bootstrap"
 function App() {
   return (
     <>
-     <h1>Welcome to proShop!</h1> 
+    <Header/>
+    <main className="py-3$ ">
+    <Container>
+    <h1>Welcome to proShop!</h1> 
+    </Container>
+     </main>
+     <Footer/>
     </>
   );
 }
