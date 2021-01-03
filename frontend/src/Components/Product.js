0cf981f3413e2ba7e70dbc31224d9d8d5daebf03
Istvan Varadi <Istvan@Amsos-MacBook-Pro.local>
2021-01-03 15:06:41 +0100

    modifications with products details

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -3,10 +3,18 @@ import {Card} from "react-bootstrap"
  const Product = ({product}) => {
     return (
         <Card className= "my-3 py-3 rounded" >
-<a href={"/product/${product.id}"}>
+<a href={"/product/${product._id}"}>
     <Card.Img src={product.image} variant="top"/>
 </a>
-        </Card>
+<Card.Body>
+<a href={"/product/${product._id}"}>
+<Card.Title as="div">
+<strong> {product.name} </strong>
+</Card.Title>
+<Card.Text as="h3">${product.price} </Card.Text>
+</a> 
+</Card.Body> 
+    </Card>
     )
 }
 
